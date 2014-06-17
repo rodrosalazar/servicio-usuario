185807828ece13645d69dbbc9bbb19e442f02d42
Jonathan Reyes <jreyes@thoughtworks.com>
2014-06-17 17:44:31 -0500

    [#98] Soportar conversion de archivos completos

--- src/main/java/ec/gob/senescyt/instituciones/carga/InstitucionesCsv.java
+++ src/main/java/ec/gob/senescyt/instituciones/carga/InstitucionesCsv.java
@@ -1,37 +1,59 @@
 package ec.gob.senescyt.instituciones.carga;
 
+import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import ec.gob.senescyt.usuario.core.Institucion;
 
+import java.io.File;
 import java.io.IOException;
 
 public class InstitucionesCsv {
 
     public static final String NOMBRE_TABLA = "instituciones";
 
-    public String filaASql(String filaCsv) throws IOException {
-        CsvMapper mapper = new CsvMapper();
-        mapper.enable(CsvParser.Feature.TRIM_SPACES);
+    public String lineaASql(String lineaCsv) throws IOException {
+        CsvMapper mapeador = new CsvMapper();
+        mapeador.enable(CsvParser.Feature.TRIM_SPACES);
 
-        Institucion institucion = mapper.readerWithSchemaFor(Institucion.class).readValue(filaCsv);
+        Institucion institucion = mapeador.readerWithSchemaFor(Institucion.class).readValue(lineaCsv);
 
+        return institucionASql(institucion);
+    }
+
+    public String archivoASql(File archivo) throws IOException {
+        CsvMapper mapeador = new CsvMapper();
+        mapeador.enable(CsvParser.Feature.TRIM_SPACES);
+        CsvSchema esquema = mapeador.schemaFor(Institucion.class).withHeader();
+
+        String resultado = "";
+
+        MappingIterator<Object> iterador = mapeador.reader(Institucion.class).with(esquema).readValues(archivo);
+        while (iterador.hasNext()) {
+            resultado += institucionASql((Institucion) iterador.next()) + "\n";
+        }
+
+        return resultado;
+    }
+
+    private String institucionASql(Institucion institucion) {
         return String.format("INSERT INTO %s VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", NOMBRE_TABLA,
-                longToSql(institucion.getId()), stringToSql(institucion.getNombre()),
-                longToSql(institucion.getIdRegimen()), stringToSql(institucion.getRegimen()),
-                longToSql(institucion.getIdEstado()), stringToSql(institucion.getEstado()),
-                longToSql(institucion.getIdCategoria()), stringToSql(institucion.getCategoria())
+                longASql(institucion.getId()), stringASql(institucion.getNombre()),
+                longASql(institucion.getIdRegimen()), stringASql(institucion.getRegimen()),
+                longASql(institucion.getIdEstado()), stringASql(institucion.getEstado()),
+                longASql(institucion.getIdCategoria()), stringASql(institucion.getCategoria())
         );
     }
 
-    private String longToSql(long number) {
+    private String longASql(long number) {
         if (number == 0) {
             return "NULL";
         }
         return String.valueOf(number);
     }
 
-    private String stringToSql(String string) {
+    private String stringASql(String string) {
         if (string == null) {
             return "NULL";
         }
--- src/test/java/ec/gob/senescyt/instituciones/carga/InstitucionesCsvTest.java
+++ src/test/java/ec/gob/senescyt/instituciones/carga/InstitucionesCsvTest.java
@@ -3,7 +3,7 @@ package ec.gob.senescyt.instituciones.carga;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,26 +18,42 @@ public class InstitucionesCsvTest {
     }
 
     @Test
-    public void debeConvertirUnaFileDeCsvASql() throws IOException {
-        String resultado = institucionesCsv.filaASql("1001,ESCUELA POLITECNICA NACIONAL,01,PUBLICA,01,VIGENTE,01,A");
+    public void debeConvertirUnaLineaDeCsvASql() throws IOException {
+        String resultado = institucionesCsv.lineaASql("1001,ESCUELA POLITECNICA NACIONAL,01,PUBLICA,01,VIGENTE,01,A");
         assertThat(resultado, is("INSERT INTO instituciones VALUES (1001, 'ESCUELA POLITECNICA NACIONAL', 1, 'PUBLICA', 1, 'VIGENTE', 1, 'A');"));
     }
 
     @Test
-    public void debeConvertirOtraFilaDeCsvASql() throws IOException {
-        String resultado = institucionesCsv.filaASql("1007,UNIVERSIDAD DE CUENCA,01,PUBLICA,01,VIGENTE,02,B");
+    public void debeConvertirOtraLineaDeCsvASql() throws IOException {
+        String resultado = institucionesCsv.lineaASql("1007,UNIVERSIDAD DE CUENCA,01,PUBLICA,01,VIGENTE,02,B");
         assertThat(resultado, is("INSERT INTO instituciones VALUES (1007, 'UNIVERSIDAD DE CUENCA', 1, 'PUBLICA', 1, 'VIGENTE', 2, 'B');"));
     }
 
     @Test
     public void debeLlenarConNullsLosCamposFaltantes() throws IOException {
-        String resultado = institucionesCsv.filaASql("2345,INSTITUTO SUPERIOR PEDAGÓGICO LOS RIOS,01,PUBLICA,01,VIGENTE");
+        String resultado = institucionesCsv.lineaASql("2345,INSTITUTO SUPERIOR PEDAGÓGICO LOS RIOS,01,PUBLICA,01,VIGENTE");
         assertThat(resultado, is("INSERT INTO instituciones VALUES (2345, 'INSTITUTO SUPERIOR PEDAGÓGICO LOS RIOS', 1, 'PUBLICA', 1, 'VIGENTE', NULL, NULL);"));
     }
 
     @Test
     public void debeRemoverEspaciosEnNombres() throws IOException {
-        String resultado = institucionesCsv.filaASql("1004,  ESCUELA POLITECNICA DEL EJERCITO   ,01,PUBLICA,03,\"CERRADA MEDIANTE RESOLUCION RPC-SO-09-NO.098-2014, DE 12 DE MARZO DE 2014, POR FUSIÓN EN LA UFA-ESPE\",01,A");
+        String resultado = institucionesCsv.lineaASql("1004,  ESCUELA POLITECNICA DEL EJERCITO   ,01,PUBLICA,03,\"CERRADA MEDIANTE RESOLUCION RPC-SO-09-NO.098-2014, DE 12 DE MARZO DE 2014, POR FUSIÓN EN LA UFA-ESPE\",01,A");
         assertThat(resultado, is("INSERT INTO instituciones VALUES (1004, 'ESCUELA POLITECNICA DEL EJERCITO', 1, 'PUBLICA', 3, 'CERRADA MEDIANTE RESOLUCION RPC-SO-09-NO.098-2014, DE 12 DE MARZO DE 2014, POR FUSIÓN EN LA UFA-ESPE', 1, 'A');"));
+    }
+
+    @Test
+    public void debeConvertirArchivosCompletos() throws IOException {
+        File archivo = File.createTempFile("prueba", "csv");
+        archivo.deleteOnExit();
+        Writer escritor = new BufferedWriter(new FileWriter(archivo, true));
+        escritor.write("CODIGO,NOMBRE,CODIGO_REGIMEN,REGIMEN,CODIGO_ESTADO,ESTADO,CODIGO_CATEGORIA,CATEGORIA\n" +
+                "1001,ESCUELA POLITECNICA NACIONAL,01,PUBLICA,01,VIGENTE,01,A\n" +
+                "1002,ESCUELA SUPERIOR POLITECNICA DE CHIMBORAZO,01,PUBLICA,01,VIGENTE,02,B\n");
+        escritor.close();
+
+        String resultado = institucionesCsv.archivoASql(archivo);
+
+        assertThat(resultado, is("INSERT INTO instituciones VALUES (1001, 'ESCUELA POLITECNICA NACIONAL', 1, 'PUBLICA', 1, 'VIGENTE', 1, 'A');\n" +
+                "INSERT INTO instituciones VALUES (1002, 'ESCUELA SUPERIOR POLITECNICA DE CHIMBORAZO', 1, 'PUBLICA', 1, 'VIGENTE', 2, 'B');\n"));
     }
 }
