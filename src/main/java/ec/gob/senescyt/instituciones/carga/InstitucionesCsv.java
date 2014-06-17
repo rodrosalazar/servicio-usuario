ed56d970fe9ce20768fb3d835603a1b727891a37
Jonathan Reyes <jreyes@thoughtworks.com>
2014-06-17 16:44:46 -0500

    [#98] Usar NULLs para los campos faltantes en la lista de Institutos

--- src/main/java/ec/gob/senescyt/instituciones/carga/InstitucionesCsv.java
+++ src/main/java/ec/gob/senescyt/instituciones/carga/InstitucionesCsv.java
@@ -7,13 +7,32 @@ import java.io.IOException;
 
 public class InstitucionesCsv {
 
+    public static final String NOMBRE_TABLA = "instituciones";
+
     public String filaASql(String filaCsv) throws IOException {
         CsvMapper mapper = new CsvMapper();
 
         Institucion institucion = mapper.readerWithSchemaFor(Institucion.class).readValue(filaCsv);
 
-        return String.format("INSERT INTO instituciones VALUES (%s, '%s', %s, '%s', %s, '%s', %s, '%s');",
-                institucion.getId(), institucion.getNombre(), institucion.getIdRegimen(), institucion.getRegimen(),
-                institucion.getIdEstado(), institucion.getEstado(), institucion.getIdCategoria(), institucion.getCategoria());
+        return String.format("INSERT INTO %s VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", NOMBRE_TABLA,
+                longToSql(institucion.getId()), stringToSql(institucion.getNombre()),
+                longToSql(institucion.getIdRegimen()), stringToSql(institucion.getRegimen()),
+                longToSql(institucion.getIdEstado()), stringToSql(institucion.getEstado()),
+                longToSql(institucion.getIdCategoria()), stringToSql(institucion.getCategoria())
+        );
+    }
+
+    private String longToSql(long number) {
+        if (number == 0) {
+            return "NULL";
+        }
+        return String.valueOf(number);
+    }
+
+    private String stringToSql(String string) {
+        if (string == null) {
+            return "NULL";
+        }
+        return "'" + string + "'";
     }
 }
--- src/test/java/ec/gob/senescyt/instituciones/carga/InstitucionesCsvTest.java
+++ src/test/java/ec/gob/senescyt/instituciones/carga/InstitucionesCsvTest.java
@@ -1,5 +1,6 @@
 package ec.gob.senescyt.instituciones.carga;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,22 +10,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class InstitucionesCsvTest {
 
+    private InstitucionesCsv institucionesCsv;
+
+    @Before
+    public void setUp() throws Exception {
+        institucionesCsv = new InstitucionesCsv();
+    }
+
     @Test
     public void debeConvertirUnaFileDeCsvASql() throws IOException {
-        InstitucionesCsv institucionesCsv = new InstitucionesCsv();
-
         String resultado = institucionesCsv.filaASql("1001,ESCUELA POLITECNICA NACIONAL,01,PUBLICA,01,VIGENTE,01,A");
-
         assertThat(resultado, is("INSERT INTO instituciones VALUES (1001, 'ESCUELA POLITECNICA NACIONAL', 1, 'PUBLICA', 1, 'VIGENTE', 1, 'A');"));
     }
 
     @Test
     public void debeConvertirOtraFilaDeCsvASql() throws IOException {
-        InstitucionesCsv institucionesCsv = new InstitucionesCsv();
-
         String resultado = institucionesCsv.filaASql("1007,UNIVERSIDAD DE CUENCA,01,PUBLICA,01,VIGENTE,02,B");
-
         assertThat(resultado, is("INSERT INTO instituciones VALUES (1007, 'UNIVERSIDAD DE CUENCA', 1, 'PUBLICA', 1, 'VIGENTE', 2, 'B');"));
+    }
 
+    @Test
+    public void debeLlenarConNullsLosCamposFaltantes() throws IOException {
+        String resultado = institucionesCsv.filaASql("2345,INSTITUTO SUPERIOR PEDAGÓGICO LOS RIOS,01,PUBLICA,01,VIGENTE");
+        assertThat(resultado, is("INSERT INTO instituciones VALUES (2345, 'INSTITUTO SUPERIOR PEDAGÓGICO LOS RIOS', 1, 'PUBLICA', 1, 'VIGENTE', NULL, NULL);"));
     }
 }
