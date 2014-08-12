dc252557889d14cef79aa4fdabe786728ceba659
Sri Prasanna <k.sriprasanna@gmail.com>
2014-08-12 16:08:09 -0500

    #266 - extendiéndose modulo de rol

--- src/main/java/ec/gob/senescyt/usuario/core/Modulo.java
+++ src/main/java/ec/gob/senescyt/usuario/core/Modulo.java
@@ -1,19 +1,8 @@
 package ec.gob.senescyt.usuario.core;
 
-public class Modulo {
-    private final int id;
-    private final String nombre;
+public class Modulo extends Rol {
 
-    public Modulo(int id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
+    public Modulo(String nombre) {
+        super(nombre);
     }
 }
--- src/test/java/ec/gob/senescyt/usuario/core/ModuloTest.java
+++ src/test/java/ec/gob/senescyt/usuario/core/ModuloTest.java
@@ -4,25 +4,22 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Random;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 public class ModuloTest{
     private String nombre;
-    private int id;
 
     @Before
     public void setUp() throws Exception {
         nombre = RandomStringUtils.random(9).toString();
-        id = new Random().nextInt(999);
     }
 
     @Test
     public void debeInicializarConIdYNombre() {
-        Modulo modulo = new Modulo(id, nombre);
-        assertThat(modulo.getId(), is(id));
+        Modulo modulo = new Modulo(nombre);
+        assertThat(modulo.getId(), is(notNullValue()));
         assertThat(modulo.getNombre(), is(nombre));
     }
 
